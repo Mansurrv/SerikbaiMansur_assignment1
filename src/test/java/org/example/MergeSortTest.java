6152657a4e724122ec7c786784bef33fee73e962
Mansurrv <buyserikov06@gmail.com>
2025-09-25 19:02:32 +0500

    Second commit add junit to pom.xml

--- src/test/java/org/example/MergeSortTest.java
+++ src/test/java/org/example/MergeSortTest.java
@@ -1,7 +1,14 @@
 package org.example;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 class MergeSortTest {
 
+    private void testSorting(int[] input, int[] expected){
+        int[] buffer = new int[input.length];
+
+        MergeSort.mergeSort(input, buffer, 0, input.length-1);
+    }
 }
